2b69b2f25b98e5d7a1ac6c052f6f13d170c61f30
Long Liang <lliang@Earth-C-137.local>
2019-11-22 22:32:31 -0800

    implemented a basic login form, view and template to render form.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 
 from app import app
+from app.forms import LoginForm
 
 
 """
@@ -33,3 +34,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', data=data)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # flash executes after submit is True; display a message to the user (add message to the template)
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
