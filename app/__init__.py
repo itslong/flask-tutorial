4fd8ce8c6b5ce98551d7e0f72e5dda9a52888b3f
Long Liang <lliang@Earth-C-137.local>
2019-11-28 00:07:58 -0800

    implemented authentication and routes.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 """
@@ -15,6 +16,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 # import down here to prevent circular imports
 # app import is from flask, not the variable
