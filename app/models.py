e88ed658d1e116c019782d7b52a092086b7e810c
Long Liang <lliang@Earth-C-137.local>
2019-11-27 18:25:43 -0800

    added and migrated a new Post db table.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
+from datetime import datetime
 from app import db
-from sqlalchemy.sql import func
 
 
 class User(db.Model):
@@ -8,8 +8,19 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     first_name = db.Column(db.String(50), nullable=True)
     last_name = db.Column(db.String(50), nullable=True)
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(140), nullable=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post Title {}>'.format(self.title)
